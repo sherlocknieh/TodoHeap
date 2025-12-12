02304b4d6c7962d06c5f309900ccd47af7591122
sherlocknieh <sherlocknieh@gmail.com>
2025-12-13 01:50:46 +0800

    引入 TailwindCSS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [tailwindcss(), vue()],
   base: process.env.NODE_ENV === 'production' ? '/TodoHeap/' : '/',
   server: {
     open: true,
